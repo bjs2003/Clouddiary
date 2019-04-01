07a67324d042547673b7490002f0ceae57cde3e6
sangeo0000 <31108679+sangeo0000@users.noreply.github.com>
2019-04-02 00:21:52 +0530

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const uname = document.getElementById("signupusername").value;
+
+//===========change pages=================================================================
 document.getElementById("changetosignupbtn").addEventListener("click", function(){
     document.getElementById("signup").style.display = "block";
     document.getElementById("login").style.display = "none";
@@ -7,3 +10,86 @@ document.getElementById("changetologinbtn").addEventListener("click", function()
     document.getElementById("signup").style.display = "none";
     document.getElementById("login").style.display = "block";
 });
+
+document.getElementById("writedatabtn").addEventListener("click", function(){
+});
+
+/*document.getElementById("enter").addEventListener("click", function(){
+    
+    
+    document.getElementById("pageone").style.display = "none";
+    document.getElementById("pagetwo").style.display = "block";
+});*/
+//======================================================================================
+
+
+
+
+
+//===========login , logout, signup===================================================
+document.getElementById("loginbtn").addEventListener("click", function(){
+    //Get Email and Pass
+    const email = document.getElementById("loginemail").value;
+    const pass = document.getElementById("loginpassword").value;
+    const auth = firebase.auth();
+    
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email,pass);
+    promise.catch(e => console.log(e.message));
+});
+
+
+//=====================================================================================
+document.getElementById("signupbtn").addEventListener("click", function(){
+    //Get Email and Pass
+    const email = document.getElementById("signupemail").value;
+    const pass = document.getElementById("signuppassword").value;
+    const auth = firebase.auth();
+    
+    //Sign up
+    const promise = auth.createUserWithEmailAndPassword(email,pass);
+    promise.catch(e => console.log(e.message));
+});
+
+
+//==================================================================================
+document.getElementById("logout").addEventListener("click", function(){
+    firebase.auth().signOut();
+});
+//===========================================================================================
+
+
+
+
+
+//===============================onAuthStateChange===========================================
+firebase.auth().onAuthStateChanged(firebaseUser => {
+    if(firebaseUser){
+        console.log(firebaseUser);
+        document.getElementById("firstpage").style.display = "none";
+        document.getElementById("homepage").style.display = "block";
+        
+        var readval = document.getElementById("readuname");
+        const uid = firebase.auth().currentUser.uid;
+        const mydbref = firebase.database().ref().child('users').child(uid);
+        
+        if(mydbref){
+            console.log("uid="+uid)
+            console.log(mydbref)
+            mydbref.on('value', snap => {
+                readval.innerHTML = snap.val().username;
+            });
+        }else{
+            console.log(uname);
+            mydbref.set({
+                username: uname
+            });
+        }
+        
+    }else{
+        console.log("not Logged in");
+        document.getElementById("firstpage").style.display = "block";
+        document.getElementById("homepage").style.display = "none";
+    }
+});
+//===============================================================================================
