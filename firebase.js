3fe7030bad9d0e36a4fc76d4fdcb3138d9c211d8
sangeo0000 <31108679+sangeo0000@users.noreply.github.com>
2019-04-01 18:18:59 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDCLpWCCie_UtBdspKhDXpoeFzT2PnXCfc",
+    authDomain: "mydb-cd2dd.firebaseapp.com",
+    databaseURL: "https://mydb-cd2dd.firebaseio.com",
+    projectId: "mydb-cd2dd",
+    storageBucket: "mydb-cd2dd.appspot.com",
+    messagingSenderId: "401886363361"
+  };
+  firebase.initializeApp(config);
